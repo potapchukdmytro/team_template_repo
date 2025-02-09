3408c4138c45bfc5dfa5319b5069f8d36b921ea1
Dmytro Potapchuk <traiget@gmail.com>
2025-02-09 10:03:19 +0200

    add user entity

---
+++ back/back/entities/UserEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace back.entities
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
