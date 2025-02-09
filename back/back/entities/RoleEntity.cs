6395d8fe2adbab82f64c673295c8a804e3d44771
Dmytro Potapchuk <traiget@gmail.com>
2025-02-09 10:05:17 +0200

    add role entity

---
+++ back/back/entities/RoleEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace back.entities
+{
+    public class RoleEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
